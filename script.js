cff6df55aff242bec6e21ba6b42abf0813957d63
Rachel Tairum <rachel.tairum@gmail.com>
2021-11-23 18:05:04 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let principal = document.getElementById('main')
 
+
 function botaoAddTarefa(){
     let botao = document.createElement('button')
     botao.id = "criar-tarefa"
@@ -7,6 +8,20 @@ function botaoAddTarefa(){
     principal.appendChild(botao)
 }
 botaoAddTarefa()
+
+function botaoApagaLista(){
+    let lista = document.querySelector('#lista-tarefas')
+    let botao = document.createElement('button')
+    botao.id = "apaga-tudo"
+    botao.innerText = "apagar lista"
+    principal.appendChild(botao)
+
+    botao.addEventListener('click', function(){
+        
+        lista.innerHTML = '';
+    })
+}
+botaoApagaLista()
 
 function criaListaDeTarefas(){
     let lista = document.createElement('ol')
@@ -29,3 +44,28 @@ function adicionaNovaTarefa(){
     })
 }
 adicionaNovaTarefa()
+
+let itemLista = document.querySelectorAll('#lista-tarefas')
+function selecionaItem(){
+    let corFundo = 'white'
+    let destaque = 'rgb(128,128,128)'
+
+    for(let i = 0; i < itemLista.length; i += 1){
+        itemLista[i].addEventListener('click', function(event){
+            event.target.style.backgroundColor = destaque
+            event.target.className = 'item selecionado'
+            
+            let selecionado = document.getElementsByClassName('selecionado')
+
+            if (selecionado.length > 1){
+                selecionado[0].style.backgroundColor = corFundo
+                selecionado[0].classList.remove('selecionado')
+                
+           } 
+     })
+    } 
+    
+}
+selecionaItem()
+
+
